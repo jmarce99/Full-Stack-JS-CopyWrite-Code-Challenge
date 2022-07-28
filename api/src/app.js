8b1915a646a233fc4ab2e5cbb76e85aa0de8f412
Marcelo Fernandez <goushart@gmail.com>
2022-07-28 16:01:21 -0300

    [Fix] Arreglo Problema de Cors

--- api/src/app.js
+++ api/src/app.js
@@ -4,30 +4,22 @@ const routes = require("./routes/index.js");
 
 // Express app
 const server = express();
-server.name = "API";
 
 // Middleware's
-server.use(morgan("dev"));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use("/", routes);
-
+server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-// Error handling endware
-server.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.log("ERROR : ", err);
-  res.status(status.send(message));
-});
+server.use("/", routes);
 
 module.exports = server;
